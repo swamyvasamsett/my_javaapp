d1a742f06044b0bc0d1c4c34326d053244420f3b
swamyvasamsett <137465562+swamyvasamsett@users.noreply.github.com>
2023-06-29 15:34:17 +0530

    Update SampleApp.java

--- src/main/java/com/kiran/SampleApp.java
+++ src/main/java/com/kiran/SampleApp.java
@@ -4,6 +4,6 @@ public class SampleApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hai swamy vasamsetti!");
-		System.out.println("Hi!");
+		System.out.println("Hi suresh!");
 	}
 }
